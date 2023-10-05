c12bcdfa6d8af7d705a478dca55021b4a33c3e46
Git Comit <git.comit@gmail.com>
2023-10-05 13:19:37 -0400

    wip

--- script/DeployMultiSig.s.sol
+++ script/DeployMultiSig.s.sol
@@ -6,25 +6,29 @@ import {Script, console} from "forge-std/Script.sol";
 import {MultiSig} from "../src/MultiSig.sol";
 
 contract DeployMultiSig is Script {
+    error DeployMultiSig__invalid_number_of_required_sigs(uint256 required, uint256 actual);
+    error DeployMultiSig__0_sigsRequired(uint256 required);
+
     address[] public owners;
+    uint256 public sigsRequired;
 
     function run() external returns (MultiSig) {
-        setOwners();
-        uint256 sigsRequired = setSigsRequired(2);
         vm.startBroadcast();
         MultiSig multiSig = new MultiSig(owners,sigsRequired);
         vm.stopBroadcast();
         return (multiSig);
     }
 
-    function setOwners() public {
-        owners.push(0xaaa3ED7b39A06E09C38ddf2252FB483AC9cDDC46);
-        owners.push(0xbbba75883E6d0fa5a7582208FfB1c16d3C802C89);
-        owners.push(0xcccC54228D6e98f500bBD5CD864Df5493aF89Fc4);
-        owners.push(0xddd2652AbcFA7a1Da38Bd156728A64053C5899A1);
-    }
-
-    function setSigsRequired(uint256 n) public pure returns (uint256 required) {
-        required = n;
+    function setOwnersAndSigs(address[] memory _owners, uint256 _sigsRequired) public {
+        for (uint256 i = 0; i < _owners.length; i++) {
+            owners.push(_owners[i]);
+        }
+        if (_sigsRequired > owners.length) {
+            revert DeployMultiSig__invalid_number_of_required_sigs(_sigsRequired, owners.length);
+        }
+        if (_sigsRequired == 0) {
+            revert DeployMultiSig__0_sigsRequired(_sigsRequired);
+        }
+        sigsRequired = _sigsRequired;
     }
 }
